feae7423952f2150ebd0ace58c31f3922748ccee
ELMANIU <132955458+ELMANIU@users.noreply.github.com>
2026-09-23 10:41:50 -0600

    Update fetch logic and change ORIGIN URL

--- worker.js
+++ worker.js
@@ -1,37 +1,15 @@
-const ORIGIN = "https://cablered.iptvperu.tv:1936/cablered/bitme_new/";
+const ORIGIN = "https://cablered.iptvperu.tv:1936/cablered/bitme_new/playlist.m3u8";
 
 export default {
- async fetch(request) {
+  async fetch(request) {
 
-  const url = new URL(request.url);
-
-  if (url.pathname.endsWith(".m3u8")) {
-
-    const res = await fetch(ORIGIN + "playlist.m3u8", {
+    const response = await fetch(ORIGIN, {
       headers:{
         "User-Agent":"Mozilla/5.0"
       }
     });
 
-    let body = await res.text();
-
-    body = body.split("\n").map(line => {
-
-      if(line && !line.startsWith("#")) {
-
-        if(line.startsWith("http")) {
-          return line;
-        }
-
-        return url.origin + "/bitme/" + line;
-      }
-
-      return line;
-
-    }).join("\n");
-
-
-    return new Response(body,{
+    return new Response(response.body,{
       headers:{
         "Content-Type":"application/vnd.apple.mpegurl",
         "Cache-Control":"no-cache"
@@ -39,30 +17,4 @@ export default {
     });
 
   }
-
-
-  if(url.pathname.startsWith("/bitme/")){
-
-    const file = url.pathname.replace("/bitme/","");
-
-    const res = await fetch(ORIGIN + file,{
-      headers:{
-        "User-Agent":"Mozilla/5.0"
-      }
-    });
-
-
-    return new Response(res.body,{
-      headers:{
-        "Content-Type":"video/mp2t",
-        "Cache-Control":"public,max-age=30"
-      }
-    });
-
-  }
-
-
-  return new Response("Worker activo");
-
- }
 }
