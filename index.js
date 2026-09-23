af2f0412e88c97db8e5c4f4a72c1cbbe77d1e25c
ELMANIU <132955458+ELMANIU@users.noreply.github.com>
2026-09-22 19:54:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
 export default {
-  async fetch(request) {
+  async fetch(request, env, ctx) {
 
     const url = new URL(request.url);
 
-    // quitar /cache/
     let path = url.pathname.replace("/cache", "");
 
     const origen = "http://fenixstream.duckdns.org";
 
     const destino = origen + path + url.search;
 
+
+    // Solo cachear segmentos TS
+    const esTS = path.endsWith(".ts");
+
+
+    const cache = caches.default;
+
+
+    if (esTS) {
+
+      const cacheKey = new Request(request.url, request);
+
+      const cached = await cache.match(cacheKey);
+
+      if (cached) {
+        return cached;
+      }
+
+
+      const respuesta = await fetch(destino, {
+        headers: {
+          "User-Agent": "Mozilla/5.0"
+        }
+      });
+
+
+      const nuevo = new Response(respuesta.body, respuesta);
+
+
+      nuevo.headers.set(
+        "Access-Control-Allow-Origin",
+        "*"
+      );
+
+
+      nuevo.headers.set(
+        "Cache-Control",
+        "public, max-age=86400"
+      );
+
+
+      ctx.waitUntil(
+        cache.put(cacheKey, nuevo.clone())
+      );
+
+
+      return nuevo;
+
+    }
+
+
+    // Playlist m3u8 siempre fresca
 
     const respuesta = await fetch(destino, {
       headers: {
@@ -27,46 +78,10 @@ export default {
     );
 
 
-    // =========================
-    // HLS PLAYLIST
-    // =========================
-
-    if (path.endsWith(".m3u8")) {
-
-      nuevo.headers.set(
-        "Cache-Control",
-        "no-cache, no-store, must-revalidate"
-      );
-
-    }
-
-
-    // =========================
-    // SEGMENTOS VIDEO
-    // =========================
-
-    else if (path.endsWith(".ts")) {
-
-      nuevo.headers.set(
-        "Cache-Control",
-        "public, max-age=86400"
-      );
-
-    }
-
-
-    // =========================
-    // OTROS ARCHIVOS
-    // =========================
-
-    else {
-
-      nuevo.headers.set(
-        "Cache-Control",
-        "public, max-age=3600"
-      );
-
-    }
+    nuevo.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
 
 
     return nuevo;
