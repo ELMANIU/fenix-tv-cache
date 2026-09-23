883bdf636bfb84c9f9320381630e1e6cc6736304
ELMANIU <132955458+ELMANIU@users.noreply.github.com>
2026-09-22 21:42:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,15 @@ export default {
   async fetch(request) {
 
     const url = new URL(request.url);
-
     let path = url.pathname.replace("/cache", "");
 
     const origen = "http://fenixstream.duckdns.org";
-
     const destino = origen + path + url.search;
 
 
-    const headers = {
-      "User-Agent": "Mozilla/5.0"
-    };
-
-
-    if (path.endsWith(".m3u8")) {
-
-      const respuesta = await fetch(destino, {
-        headers
-      });
-
-      const nuevo = new Response(respuesta.body, respuesta);
-
-      nuevo.headers.set(
-        "Cache-Control",
-        "no-cache, no-store, must-revalidate"
-      );
-
-      nuevo.headers.set(
-        "Access-Control-Allow-Origin",
-        "*"
-      );
-
-      return nuevo;
-
-    }
-
-
     const respuesta = await fetch(destino, {
-      headers,
-      cf:{
-        cacheEverything:true,
-        cacheTtl:86400
+      headers:{
+        "User-Agent":"Mozilla/5.0"
       }
     });
 
@@ -51,14 +19,34 @@ export default {
 
 
     nuevo.headers.set(
-      "Cache-Control",
-      "public, max-age=86400"
-    );
-
-    nuevo.headers.set(
       "Access-Control-Allow-Origin",
       "*"
     );
+
+
+    if (path.endsWith(".m3u8")) {
+
+      nuevo.headers.set(
+        "Cache-Control",
+        "no-store, no-cache, must-revalidate, max-age=0"
+      );
+
+      nuevo.headers.set(
+        "CDN-Cache-Control",
+        "no-store"
+      );
+
+    }
+
+
+    if (path.endsWith(".ts")) {
+
+      nuevo.headers.set(
+        "Cache-Control",
+        "public, max-age=86400"
+      );
+
+    }
 
 
     return nuevo;
